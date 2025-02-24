f99cead61020575afc2a703e65ddc13d37a2d22d
SkyFurY44 <46328171+SkyFurY44@users.noreply.github.com>
2025-02-24 16:52:43 +0100

    Create EnchereRepository.java

---
+++ encheres/src/main/java/fr/campus/eni/dal/EnchereRepository.java
@@ -0,0 +1,10 @@
+package fr.campus.eni.dal;
+
+package fr.campus.eni.bo.Enchere;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EnchereRepository extends ICrudRepository<Enchere> {
+
+}
