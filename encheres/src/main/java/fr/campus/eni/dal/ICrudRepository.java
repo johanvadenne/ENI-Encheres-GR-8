eeb7e64780408b0819d9cf7500a56a4fd487fc98
SkyFurY44 <46328171+SkyFurY44@users.noreply.github.com>
2025-02-24 16:52:46 +0100

    Create ICrudRepository.java

---
+++ encheres/src/main/java/fr/campus/eni/dal/ICrudRepository.java
@@ -0,0 +1,18 @@
+package fr.campus.eni.dal;
+
+package fr.campus.eni.bo.Enchere;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICrudRepository<T> {
+    void add(T entity);
+
+    public List<T> getAll();
+
+    Optional<T> getById(int id);
+
+    void update(T entity);
+
+    void delete(int id);
+}
