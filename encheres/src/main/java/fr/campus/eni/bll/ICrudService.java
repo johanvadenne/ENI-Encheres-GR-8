797660545822d01461921624187d6302245ff5c7
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-02-25 09:58:20 +0100

    Create ICrudService.java

---
+++ encheres/src/main/java/fr/campus/eni/bll/ICrudService.java
@@ -0,0 +1,19 @@
+package fr.campus.eni.bll;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICrudService<T> {
+    void add(T entity);
+
+    public List<T> getAll();
+
+    Optional<T> getById(int id);
+
+    void update(T entity);
+    
+    void save(T entity);
+
+    void delete(int id);
+
+}
