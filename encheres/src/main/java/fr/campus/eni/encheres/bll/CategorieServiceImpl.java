fa6b4c126ea062c1beecb0f0b8a68abde5bf0e8f
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-02-25 16:17:27 +0100

    Create CategorieServiceImpl.java

---
+++ encheres/src/main/java/fr/campus/eni/encheres/bll/CategorieServiceImpl.java
@@ -0,0 +1,69 @@
+package fr.campus.eni.encheres.bll;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import fr.campus.eni.encheres.bo.Categorie;
+import fr.campus.eni.encheres.dal.CategorieRepositoryImpl;
+import fr.campus.eni.encheres.exceptions.ExeptionEchere;
+
+@Service
+public class CategorieServiceImpl implements ICrudService<Categorie> {
+
+    private final CategorieRepositoryImpl CategorieRepositoryImpl;
+    private final PasswordEncoder passwordEncoder;
+
+    public CategorieServiceImpl(
+        CategorieRepositoryImpl categorieRepositoryImpl,
+        PasswordEncoder passwordEncoder
+    ) {
+        this.CategorieRepositoryImpl = categorieRepositoryImpl;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void add(Categorie categorie) {
+        CategorieRepositoryImpl.add(categorie);
+    }
+
+    @Override
+    public List<Categorie> getAll() {
+        return CategorieRepositoryImpl.getAll();
+    }
+
+    @Override
+    public Optional<Categorie> getById(int id) {
+        return CategorieRepositoryImpl.getById(id);
+    }
+
+    @Override
+    public void update(Categorie client) {
+        Optional<Categorie> clientOpt = getById(client.getNoCategorie());
+        if (clientOpt.isPresent()) {
+            CategorieRepositoryImpl.update(client);
+        } else {
+            //TODO gerer l'erreur
+            throw new ExeptionEchere();
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        CategorieRepositoryImpl.delete(id);
+    }
+    
+    
+    @Override
+    public void save(Categorie entity) {
+
+        if (entity.getNoCategorie() == null) {
+            this.add(entity);
+            return;
+        }
+        this.update(entity);
+
+    }
+}
