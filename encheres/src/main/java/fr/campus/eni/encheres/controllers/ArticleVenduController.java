6190361009ae91f00791fdd667548f9b98de888f
SkyFurY44 <46328171+SkyFurY44@users.noreply.github.com>
2025-02-26 11:31:56 +0100

    Create ArticleVenduController.java

---
+++ encheres/src/main/java/fr/campus/eni/encheres/controllers/ArticleVenduController.java
@@ -0,0 +1,26 @@
+package fr.campus.eni.encheres.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import fr.campus.eni.encheres.bll.ArticleVenduServiceImpl;
+import fr.campus.eni.encheres.bo.ArticleVendu;
+
+@Controller
+public class ArticleVenduController {
+
+    @Autowired
+    private ArticleVenduServiceImpl articleVenduServiceImpl;
+
+   @GetMapping("/listeVentes")
+    public String listerVentes(Model model) {
+        List<ArticleVendu> articles = articleVenduServiceImpl.getAll();
+        System.out.println(articles);
+        model.addAttribute("lesArticlesVendus", articles);
+        return "pages/ventes/listeVente";  // Le nom de votre fichier HTML
+    }
+}
