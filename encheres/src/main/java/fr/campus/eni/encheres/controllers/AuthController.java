6ee92dd9953f9dd2b222a5213401c97e2807489c
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-02-26 09:51:34 +0100

    Update AuthController.java

--- encheres/src/main/java/fr/campus/eni/encheres/controllers/AuthController.java
+++ encheres/src/main/java/fr/campus/eni/encheres/controllers/AuthController.java
@@ -1,10 +1,24 @@
 package fr.campus.eni.encheres.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import fr.campus.eni.encheres.bll.UtilisateurServiceImpl;
+import fr.campus.eni.encheres.bo.Utilisateur;
+import jakarta.validation.Valid;
 
 @Controller
 public class AuthController {
+
+    private UtilisateurServiceImpl UtilisateurServiceImpl;
+
+    AuthController(UtilisateurServiceImpl UtilisateurServiceImpl) {
+        this.UtilisateurServiceImpl = UtilisateurServiceImpl;
+    }
 
     @GetMapping("/login")
     public String afficherFormulaireLogin() {
@@ -16,5 +30,23 @@ public class AuthController {
     public String afficherFormulaireRegister() {
         // Retourne la vue Thymeleaf du formulaire de connexion
         return "pages/utilisateurs/formulaire-inscription";
+    }
+
+    @PostMapping("/register")
+    public String enregistrerNouvelUtilisateur(
+            Model model,
+            @Valid @ModelAttribute("utilisateur") Utilisateur utilisateur,
+            BindingResult resultatValidation
+    ) {
+        if (resultatValidation.hasErrors()) {
+            return "pages/utilisateurs/formulaire-inscription";
+        }
+        // Sauvegarde
+        UtilisateurServiceImpl.save(utilisateur);
+
+        System.out.println("Utilisateur enregistré : " + utilisateur.getPrenom() + " " + utilisateur.getNom());
+
+        // Redirection login
+        return "redirect:/login";
     }
 }
