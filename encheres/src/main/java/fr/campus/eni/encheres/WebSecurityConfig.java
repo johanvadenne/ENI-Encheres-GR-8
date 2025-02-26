7d3cd7b07f14ff044b277d5a69658e4762d309f2
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-02-26 09:47:29 +0100

    Update WebSecurityConfig.java

--- encheres/src/main/java/fr/campus/eni/encheres/WebSecurityConfig.java
+++ encheres/src/main/java/fr/campus/eni/encheres/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/utilisateurs/login", "/utilisateurs/register").permitAll()
+				.requestMatchers("/utilisateurs/login", "/utilisateurs/register", "/utilisateurs/test").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
