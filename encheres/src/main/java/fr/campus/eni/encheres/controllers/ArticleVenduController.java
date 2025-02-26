6b448155350c8f223897ae0bc1381edb07c69410
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-02-26 13:21:53 +0100

    Update ArticleVenduController.java

--- encheres/src/main/java/fr/campus/eni/encheres/controllers/ArticleVenduController.java
+++ encheres/src/main/java/fr/campus/eni/encheres/controllers/ArticleVenduController.java
@@ -1,5 +1,6 @@
 package fr.campus.eni.encheres.controllers;
 
+import java.security.Principal;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -7,15 +8,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.campus.eni.encheres.bll.ArticleVenduServiceImpl;
+import fr.campus.eni.encheres.bll.CategorieServiceImpl;
+import fr.campus.eni.encheres.bll.UtilisateurServiceImpl;
 import fr.campus.eni.encheres.bo.ArticleVendu;
+import fr.campus.eni.encheres.bo.Categorie;
+import fr.campus.eni.encheres.bo.Retrait;
+import fr.campus.eni.encheres.bo.Utilisateur;
 
 @Controller
 public class ArticleVenduController {
 
     @Autowired
     private ArticleVenduServiceImpl articleVenduServiceImpl;
+
+    private final ArticleVenduServiceImpl articleService;
+    private final CategorieServiceImpl categorieService;
+    private final UtilisateurServiceImpl utilisateurService;
+
+    public ArticleVenduController(ArticleVenduServiceImpl articleService, CategorieServiceImpl categorieService, UtilisateurServiceImpl utilisateurService) {
+        this.articleService = articleService;
+        this.categorieService = categorieService;
+        this.utilisateurService = utilisateurService;
+    }
 
     @GetMapping("/listeVentes")
     public String listerVentes(Model model) {
@@ -27,5 +45,25 @@ public class ArticleVenduController {
         }
         model.addAttribute("lesArticlesVendus", articles);
         return "pages/ventes/listeVente";  // Le nom de votre fichier HTML
+    }
+
+
+
+    @GetMapping("/creer-vente")
+    public String afficherFormulaireVente(Model model, Principal principal) {
+        List<Categorie> categories = categorieService.getAll();
+        
+        model.addAttribute("articleVendu", new ArticleVendu());
+        model.addAttribute("categories", categories);
+        
+
+        return "pages/ventes/creerVente";
+    }
+
+    @PostMapping("/creer-vente")
+    public String soumettreArticle(@ModelAttribute("articleVendu") ArticleVendu articleVendu) {
+        System.out.println(articleVendu);
+        articleService.save(articleVendu);
+        return "redirect:/listeVentes";
     }
 }
