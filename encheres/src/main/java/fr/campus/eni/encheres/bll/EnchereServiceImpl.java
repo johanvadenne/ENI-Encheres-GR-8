213875d32c0e8321d04447ba3f5b97a780613f3c
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-02-27 16:39:28 +0100

    Update EnchereServiceImpl.java

--- encheres/src/main/java/fr/campus/eni/encheres/bll/EnchereServiceImpl.java
+++ encheres/src/main/java/fr/campus/eni/encheres/bll/EnchereServiceImpl.java
@@ -1,70 +1,112 @@
 package fr.campus.eni.encheres.bll;
 
+import fr.campus.eni.encheres.bo.ArticleVendu;
 import fr.campus.eni.encheres.bo.Enchere;
+import fr.campus.eni.encheres.bo.Utilisateur;
+import fr.campus.eni.encheres.dal.ArticleVenduRepositoryImpl;
 import fr.campus.eni.encheres.dal.EnchereRepositoryImpl;
 import fr.campus.eni.encheres.exceptions.ExeptionEchere;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.campus.eni.encheres.dal.UtilisateurRepositoryImpl;
 
 @Service
 public class EnchereServiceImpl implements ICrudService<Enchere> {
 
-  private final EnchereRepositoryImpl EnchereRepositoryImpl;
-  private final PasswordEncoder passwordEncoder;
+    private final EnchereRepositoryImpl EnchereRepositoryImpl;
+    private final PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private ArticleVenduRepositoryImpl articleRepository;
 
-  public EnchereServiceImpl(
-      EnchereRepositoryImpl enchereRepositoryImpl, PasswordEncoder passwordEncoder) {
-    this.EnchereRepositoryImpl = enchereRepositoryImpl;
-    this.passwordEncoder = passwordEncoder;
-  }
+    @Autowired
+    private UtilisateurRepositoryImpl UtilisateurRepository;
 
-  @Override
-  public void add(Enchere enchere) {
-    EnchereRepositoryImpl.add(enchere);
-  }
-
-  @Override
-  public List<Enchere> getAll() {
-    return EnchereRepositoryImpl.getAll();
-  }
-
-  public List<Enchere> getByNoArticle(Integer noArticle) {
-    return EnchereRepositoryImpl.getByNoArticle(noArticle);
-  }
-
-  public List<Enchere> getEnhereValidByNoUtilisateur(Integer noUtilisateur) {
-    return EnchereRepositoryImpl.getEnhereValidByNoUtilisateur(noUtilisateur);
-  }
-
-  @Override
-  public Optional<Enchere> getById(int id) {
-    return EnchereRepositoryImpl.getById(id);
-  }
-
-  @Override
-  public void update(Enchere client) {
-    Optional<Enchere> clientOpt = getById(client.getNoEnchere());
-    if (clientOpt.isPresent()) {
-      EnchereRepositoryImpl.update(client);
-    } else {
-      throw new ExeptionEchere();
+    public EnchereServiceImpl(
+            EnchereRepositoryImpl enchereRepositoryImpl, PasswordEncoder passwordEncoder) {
+        this.EnchereRepositoryImpl = enchereRepositoryImpl;
+        this.passwordEncoder = passwordEncoder;
     }
-  }
 
-  @Override
-  public void delete(int id) {
-    EnchereRepositoryImpl.delete(id);
-  }
-
-  @Override
-  public void save(Enchere entity) {
-
-    if (entity.getNoEnchere() == null) {
-      this.add(entity);
-      return;
+    @Override
+    public void add(Enchere enchere) {
+        EnchereRepositoryImpl.add(enchere);
     }
-    this.update(entity);
-  }
+
+    @Override
+    public List<Enchere> getAll() {
+        return EnchereRepositoryImpl.getAll();
+    }
+
+    public List<Enchere> getByNoArticle(Integer noArticle) {
+        return EnchereRepositoryImpl.getByNoArticle(noArticle);
+    }
+
+    public List<Enchere> getEnhereValidByNoUtilisateur(Integer noUtilisateur) {
+        return EnchereRepositoryImpl.getEnhereValidByNoUtilisateur(noUtilisateur);
+    }
+
+    @Override
+    public Optional<Enchere> getById(int id) {
+        return EnchereRepositoryImpl.getById(id);
+    }
+
+    @Override
+    public void update(Enchere client) {
+        Optional<Enchere> clientOpt = getById(client.getNoEnchere());
+        if (clientOpt.isPresent()) {
+            EnchereRepositoryImpl.update(client);
+        } else {
+            throw new ExeptionEchere();
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        EnchereRepositoryImpl.delete(id);
+    }
+
+    @Override
+    public void save(Enchere entity) {
+
+        if (entity.getNoEnchere() == null) {
+            this.add(entity);
+            return;
+        }
+        this.update(entity);
+    }
+
+    @Transactional
+    public void verifierEtCloturerEncheres() {
+        List<ArticleVendu> articlesACloturer = articleRepository.findByDateFinEncheres();
+
+        for (ArticleVendu article : articlesACloturer) {
+            Optional<Enchere> meilleureEnchereOpt = EnchereRepositoryImpl.getLastEnchereByArticle(article.getNoArticle());
+
+            if (meilleureEnchereOpt.isPresent()) {
+                Enchere meilleureEnchere = meilleureEnchereOpt.get();
+                Utilisateur vendeur = article.getVendeur();
+                Utilisateur acheteur = UtilisateurRepository.getById(meilleureEnchere.getNoUtilisateur()).get();
+
+                vendeur.setCredit(vendeur.getCredit()+meilleureEnchere.getMontantEnchere());
+              article.setPrixVente(meilleureEnchere.getMontantEnchere());
+              acheteur.setCredit(acheteur.getCredit() - meilleureEnchere.getMontantEnchere());
+
+            UtilisateurRepository.updateCredit(vendeur);
+            UtilisateurRepository.updateCredit(acheteur);
+            
+            }
+            else {
+              article.setPrixVente(0);
+            }
+        }
+    }
 }
