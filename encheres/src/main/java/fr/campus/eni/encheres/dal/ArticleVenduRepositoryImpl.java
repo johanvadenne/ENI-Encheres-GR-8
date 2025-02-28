a805994a8e7c4f8f6313fe1561ad4ee29ef0702e
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-02-28 09:58:20 +0100

    Update ArticleVenduRepositoryImpl.java

--- encheres/src/main/java/fr/campus/eni/encheres/dal/ArticleVenduRepositoryImpl.java
+++ encheres/src/main/java/fr/campus/eni/encheres/dal/ArticleVenduRepositoryImpl.java
@@ -15,176 +15,118 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import fr.campus.eni.encheres.bo.ArticleVendu;
-import fr.campus.eni.encheres.bo.Categorie;
 import fr.campus.eni.encheres.bo.Retrait;
-import fr.campus.eni.encheres.bo.Utilisateur;
 
 @Repository
 public class ArticleVenduRepositoryImpl implements ICrudRepository<ArticleVendu> {
 
-  Logger logger = LoggerFactory.getLogger(ArticleVenduRepositoryImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(ArticleVenduRepositoryImpl.class);
 
-  private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-  private JdbcTemplate jdbcTemplate;
-  private CategorieRepositoryImpl categorieRepositoryImpl;
-  private RetraitRepositoryImpl retraitRepositoryImpl;
-  private UtilisateurRepositoryImpl utilisateurRepositoryImpl;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
+    private final CategorieRepositoryImpl categorieRepository;
+    private final RetraitRepositoryImpl retraitRepository;
+    private final UtilisateurRepositoryImpl utilisateurRepository;
 
-  public ArticleVenduRepositoryImpl(
-      NamedParameterJdbcTemplate namedParameterJdbcTemplate,
-      JdbcTemplate jdbcTemplate,
-      CategorieRepositoryImpl categorieRepositoryImpl,
-      RetraitRepositoryImpl retraitRepositoryImpl,
-      UtilisateurRepositoryImpl utilisateurRepositoryImpl) {
-    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-    this.jdbcTemplate = jdbcTemplate;
-    this.categorieRepositoryImpl = categorieRepositoryImpl;
-    this.retraitRepositoryImpl = retraitRepositoryImpl;
-    this.utilisateurRepositoryImpl = utilisateurRepositoryImpl;
-  }
-
-  @Override
-  public void add(ArticleVendu unArticleVendu) {
-    String sql =
-        """
-    INSERT INTO articles_vendus
-        (nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie)
-     VALUES
-        (:nomArticle, :description, :dateDebutEncheres, :dateFinEncheres, :prixInitial, :prixVente, :noUtilisateur, :no_categorie)
-    RETURNING no_article
-""";
-
-    KeyHolder keyHolder = new GeneratedKeyHolder();
-
-    namedParameterJdbcTemplate.update(
-        sql,
-        new BeanPropertySqlParameterSource(unArticleVendu),
-        keyHolder,
-        new String[] {"no_article"});
-
-    // Récupère l'ID généré et le définit dans l'objet
-    Number generatedId = keyHolder.getKey();
-    if (generatedId != null) {
-      unArticleVendu.setNoArticle(generatedId.intValue());
-    }
-  }
-
-  @Override
-  public List<ArticleVendu> getAll() {
-    String sql =
-        """
-    select
-      no_article, nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie, etatvente
-from
-      articles_vendus
-""";
-    List<ArticleVendu> articleVendus =
-        namedParameterJdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ArticleVendu.class));
-    for (ArticleVendu articleVendu : articleVendus) {
-      String idCategorieSQL = "select no_categorie from articles_vendus where no_article = ?";
-      int idCat =
-          jdbcTemplate.queryForObject(idCategorieSQL, Integer.class, articleVendu.getNoArticle());
-      Categorie categorie = categorieRepositoryImpl.getById(idCat).get();
-      articleVendu.setCategorie(categorie);
-
-      Optional<Retrait> retrait = retraitRepositoryImpl.getById(articleVendu.getNoArticle());
-      if (retrait.isPresent()) {
-        articleVendu.setRetrait(retrait.get());
-      } else {
-        articleVendu.setRetrait(new Retrait("", "", ""));
-        logger.warn("Retrait non trouvé pour l'article " + articleVendu.getNoArticle());
-      }
-
-      Utilisateur utilisateur = utilisateurRepositoryImpl.getById(articleVendu.getNoUtilisateur()).get();
-      articleVendu.setVendeur(utilisateur);
+    public ArticleVenduRepositoryImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate,
+                                      JdbcTemplate jdbcTemplate,
+                                      CategorieRepositoryImpl categorieRepository,
+                                      RetraitRepositoryImpl retraitRepository,
+                                      UtilisateurRepositoryImpl utilisateurRepository) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+        this.jdbcTemplate = jdbcTemplate;
+        this.categorieRepository = categorieRepository;
+        this.retraitRepository = retraitRepository;
+        this.utilisateurRepository = utilisateurRepository;
     }
 
-    return articleVendus;
-  }
-
-  public List<ArticleVendu> findByDateFinEncheres() {
-    String sql =
-        """
-    select
-      no_article, nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie, etatvente
-      from
-      articles_vendus
-      WHERE date_fin_encheres < NOW()
-""";
-    List<ArticleVendu> articleVendus =
-        namedParameterJdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ArticleVendu.class));
-    for (ArticleVendu articleVendu : articleVendus) {
-      String idCategorieSQL = "select no_categorie from articles_vendus where no_article = ?";
-      int idCat =
-          jdbcTemplate.queryForObject(idCategorieSQL, Integer.class, articleVendu.getNoArticle());
-      Categorie categorie = categorieRepositoryImpl.getById(idCat).get();
-      articleVendu.setCategorie(categorie);
-
-      Optional<Retrait> retrait = retraitRepositoryImpl.getById(articleVendu.getNoArticle());
-      if (retrait.isPresent()) {
-        articleVendu.setRetrait(retrait.get());
-      } else {
-        articleVendu.setRetrait(new Retrait("", "", ""));
-      }
-
-      Utilisateur utilisateur = utilisateurRepositoryImpl.getById(articleVendu.getNoUtilisateur()).get();
-      articleVendu.setVendeur(utilisateur);
-    }
-
-    return articleVendus;
-  }
-
-  @Override
-  public Optional<ArticleVendu> getById(int id) {
-    String sql =
-        """
-select
-  no_article, nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie, etatvente
-from
-  articles_vendus where no_article = ?
-""";
-    ArticleVendu articleVendu = null;
-    try {
-      articleVendu =
-          jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(ArticleVendu.class), id);
-    } catch (DataAccessException exc) {
-      exc.printStackTrace();
-      logger.warn(exc.getMessage());
-    }
-
-    return Optional.ofNullable(articleVendu);
-  }
-
-  @Override
-  public void update(ArticleVendu articleVendu) {
-    String sql =
-        """
-        update
-          articles_vendus
-            set
-           nom_article=:nomArticle,
-           description=:description,
-           date_debut_encheres=:dateDebutEncheres,
-           date_fin_encheres=:dateFinEncheres,
-           prix_initial=:prixInitial,
-           prix_vente=:prixVente,
-           no_utilisateur=:noUtilisateur,
-           etatvente=:etatvente
-          where no_article = :noArticle
+    @Override
+    public void add(ArticleVendu article) {
+        String sql = """
+            INSERT INTO articles_vendus
+            (nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie)
+            VALUES (:nomArticle, :description, :dateDebutEncheres, :dateFinEncheres, :prixInitial, :prixVente, :noUtilisateur, :noCategorie)
+            RETURNING no_article
         """;
-    int nbRows =
-        namedParameterJdbcTemplate.update(sql, new BeanPropertySqlParameterSource(articleVendu));
-    if (nbRows != 1) {
-      throw new RuntimeException("La modification du client a échouée : " + articleVendu);
+        
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(sql, new BeanPropertySqlParameterSource(article), keyHolder, new String[]{"no_article"});
+        
+        Optional.ofNullable(keyHolder.getKey()).ifPresent(id -> article.setNoArticle(id.intValue()));
     }
-  }
 
-  @Override
-  public void delete(int id) {
-    String sql = "delete from articles_vendus where no_article = ? ";
-    int nbRows = jdbcTemplate.update(sql, id);
-    if (nbRows != 1) {
-      throw new RuntimeException("La suppression de l'articleVendu a échouée : id= " + id);
+    @Override
+    public List<ArticleVendu> getAll() {
+        String sql = "SELECT * FROM articles_vendus";
+        List<ArticleVendu> articles = namedParameterJdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ArticleVendu.class));
+        articles.forEach(this::hydrateArticle);
+        return articles;
     }
-  }
+
+    public List<ArticleVendu> findByDateFinEncheres() {
+        String sql = "SELECT * FROM articles_vendus WHERE date_fin_encheres < NOW()";
+        List<ArticleVendu> articles = namedParameterJdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ArticleVendu.class));
+        articles.forEach(this::hydrateArticle);
+        return articles;
+    }
+
+    @Override
+    public Optional<ArticleVendu> getById(int id) {
+        String sql = "SELECT * FROM articles_vendus WHERE no_article = ?";
+        try {
+            ArticleVendu article = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(ArticleVendu.class), id);
+            hydrateArticle(article);
+            return Optional.ofNullable(article);
+        } catch (DataAccessException e) {
+            logger.warn("Article non trouvé: {}", e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void update(ArticleVendu article) {
+        String sql = """
+            UPDATE articles_vendus SET
+            nom_article = :nomArticle,
+            description = :description,
+            date_debut_encheres = :dateDebutEncheres,
+            date_fin_encheres = :dateFinEncheres,
+            prix_initial = :prixInitial,
+            prix_vente = :prixVente,
+            no_utilisateur = :noUtilisateur,
+            etatvente = :etatVente,
+            no_categorie = :noCategorie
+            WHERE no_article = :noArticle
+        """;
+        
+        int rows = namedParameterJdbcTemplate.update(sql, new BeanPropertySqlParameterSource(article));
+        if (rows != 1) {
+            throw new RuntimeException("Échec de la mise à jour de l'article: " + article);
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        String sql = "DELETE FROM articles_vendus WHERE no_article = ?";
+        int rows = jdbcTemplate.update(sql, id);
+        if (rows != 1) {
+            throw new RuntimeException("Échec de la suppression de l'article, ID: " + id);
+        }
+    }
+
+    private void hydrateArticle(ArticleVendu article) {
+        if (article == null) return;
+
+        categorieRepository.getById(article.getNoCategorie())
+            .ifPresent(article::setCategorie);
+
+        retraitRepository.getById(article.getNoArticle())
+            .ifPresentOrElse(article::setRetrait,
+                () -> {
+                    article.setRetrait(new Retrait("", "", ""));
+                });
+
+        utilisateurRepository.getById(article.getNoUtilisateur())
+            .ifPresent(article::setVendeur);
+    }
 }
