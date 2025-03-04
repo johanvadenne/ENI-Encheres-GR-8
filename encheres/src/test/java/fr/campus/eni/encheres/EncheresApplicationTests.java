8b1ae9ece4af2dd5848af6c37c5fdf89d19ff933
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-03-04 15:57:42 +0100

    Update EncheresApplicationTests.java

--- encheres/src/test/java/fr/campus/eni/encheres/EncheresApplicationTests.java
+++ encheres/src/test/java/fr/campus/eni/encheres/EncheresApplicationTests.java
@@ -1,10 +1,23 @@
 package fr.campus.eni.encheres;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootTest
 class EncheresApplicationTests {
+
+  static Dotenv dotenv;
+
+    @BeforeAll
+    static void setup() {
+        dotenv = Dotenv.load(); // Charge le fichier .env automatiquement
+        System.setProperty("DATASOURCE_URL", dotenv.get("DATASOURCE_URL"));
+        System.setProperty("DATASOURCE_USERNAME", dotenv.get("DATASOURCE_USERNAME"));
+        System.setProperty("DATASOURCE_PASSWORD", dotenv.get("DATASOURCE_PASSWORD"));
+    }
 
   @Test
   void contextLoads() {}
