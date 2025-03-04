25065b809662f320c02c36e2b4262e1316689c89
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-03-04 14:28:56 +0100

    Update create_bd.sql

--- doc/bdd/create_bd.sql
+++ doc/bdd/create_bd.sql
@@ -19,7 +19,8 @@ CREATE TABLE UTILISATEURS (
     ville          VARCHAR(50) NOT NULL,
     mot_de_passe   VARCHAR(256) NOT NULL,
     credit         INTEGER NOT NULL,
-    administrateur BOOLEAN NOT NULL -- Utilisation de BOOLEAN pour le type bit
+    administrateur BOOLEAN NOT NULL DEFAULT FALSE, -- Utilisation de BOOLEAN pour le type bit
+    desactive BOOLEAN DEFAULT FALSE
 );
 
 -- Création de la table ARTICLES_VENDUS
