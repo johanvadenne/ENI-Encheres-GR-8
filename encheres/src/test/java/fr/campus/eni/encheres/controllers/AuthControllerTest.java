56dbcb391bf8feae68a79118c34755fb9b31a7df
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-03-04 16:46:32 +0100

    Update AuthControllerTest.java

--- encheres/src/test/java/fr/campus/eni/encheres/controllers/AuthControllerTest.java
+++ encheres/src/test/java/fr/campus/eni/encheres/controllers/AuthControllerTest.java
@@ -1,29 +1,23 @@
 package fr.campus.eni.encheres.controllers;
 
-import fr.campus.eni.encheres.bll.UtilisateurServiceImpl;
-import fr.campus.eni.encheres.bo.Utilisateur;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-@WebMvcTest(AuthController.class)
-@AutoConfigureMockMvc(addFilters = false) // Désactive les filtres de sécurité dans le test
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+
+
+@SpringBootTest  // Charge tout le contexte Spring Boot
+@AutoConfigureMockMvc(addFilters = false)
 public class AuthControllerTest {
 
     @Autowired
@@ -31,23 +25,19 @@ public class AuthControllerTest {
 
     static Dotenv dotenv;
 
-    @BeforeAll
-    static void setup() {
-        dotenv = Dotenv.load(); // Charge le fichier .env automatiquement
-        System.setProperty("DATASOURCE_URL", dotenv.get("DATASOURCE_URL"));
-        System.setProperty("DATASOURCE_USERNAME", dotenv.get("DATASOURCE_USERNAME"));
-        System.setProperty("DATASOURCE_PASSWORD", dotenv.get("DATASOURCE_PASSWORD"));
-    }
-
-    // On simule le service utilisé par le contrôleur
-    @MockBean
-    private UtilisateurServiceImpl utilisateurServiceImpl;
+  @BeforeAll
+  static void setup() {
+      dotenv = Dotenv.load();
+      System.setProperty("DATASOURCE_URL", dotenv.get("DATASOURCE_URL"));
+      System.setProperty("DATASOURCE_USERNAME", dotenv.get("DATASOURCE_USERNAME"));
+      System.setProperty("DATASOURCE_PASSWORD", dotenv.get("DATASOURCE_PASSWORD"));
+  }
 
     @Test
     public void testAfficherFormulaireLogin() throws Exception {
-        mockMvc.perform(get("/login"))
+        
+        mockMvc.perform(get("/login").with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("pages/utilisateurs/formulaire-connexion"));
     }
-
 }
