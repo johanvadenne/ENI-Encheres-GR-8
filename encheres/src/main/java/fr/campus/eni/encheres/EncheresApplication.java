d73a963a4e7bfba902cc1780a3dc8581a885edaa
Jojo Lapin <106351932+johanvadenne@users.noreply.github.com>
2025-03-04 14:27:16 +0100

    Update EncheresApplication.java

--- encheres/src/main/java/fr/campus/eni/encheres/EncheresApplication.java
+++ encheres/src/main/java/fr/campus/eni/encheres/EncheresApplication.java
@@ -1,14 +1,36 @@
 package fr.campus.eni.encheres;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableScheduling
-public class EncheresApplication {
+public class EncheresApplication implements CommandLineRunner {
+
+    private final Dotenv dotenv;
+
+    public EncheresApplication(Dotenv dotenv) {
+        this.dotenv = dotenv;
+    }
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("DATASOURCE_URL", dotenv.get("DATASOURCE_URL"));
+    System.setProperty("DATASOURCE_USERNAME", dotenv.get("DATASOURCE_USERNAME"));
+    System.setProperty("DATASOURCE_PASSWORD", dotenv.get("DATASOURCE_PASSWORD"));
     SpringApplication.run(EncheresApplication.class, args);
+  }
+
+  
+  @Override
+  public void run(String... args) {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("DATASOURCE_URL", dotenv.get("DATASOURCE_URL"));
+    System.setProperty("DATASOURCE_USERNAME", dotenv.get("DATASOURCE_USERNAME"));
+    System.setProperty("DATASOURCE_PASSWORD", dotenv.get("DATASOURCE_PASSWORD"));
   }
 }
